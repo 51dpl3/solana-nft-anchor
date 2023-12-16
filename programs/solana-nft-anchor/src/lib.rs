0df63f766f18c5e7cd6dbb491589b191db73471b
51dpl3 <153866394+51dpl3@users.noreply.github.com>
2023-12-16 02:24:49 +0000

    Updated

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -12,7 +12,7 @@ use mpl_token_metadata::{
     state::DataV2,
 };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("DnjwprB8GxjKQgFMFx8FtvaM43jSAEv9FbGwJUz8PDJi");
 #[program]
 pub mod solana_nft_anchor {
 
